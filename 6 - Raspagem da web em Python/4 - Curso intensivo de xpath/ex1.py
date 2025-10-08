071db159d944a779592936919e277c25f621a5b6
Bismarck <brunowasborn2rock@gmail.com>
2025-10-08 15:49:55 -0300

    web scraping more

---
+++ 6 - Raspagem da web em Python/4 - Curso intensivo de xpath/ex1.py
@@ -0,0 +1,30 @@
+html = """
+<html>
+
+  <body>
+
+    <div>
+
+      <p>Good Luck!</p>
+
+      <p>Not here...</p>
+
+    </div>
+
+    <div>
+
+      <p>Where am I?</p>
+
+    </div>
+
+  </body>
+
+</html>
+"""
+
+# Fill in the blank
+xpath = '/html/body/div[2]/p'
+
+'''
+O código acima demonstra como usar XPath para selecionar um elemento específico em um documento HTML. Ele seleciona o segundo parágrafo dentro do segundo div no corpo do HTML.
+'''
---
+++ 6 - Raspagem da web em Python/4 - Curso intensivo de xpath/ex2.py
@@ -0,0 +1,8 @@
+from ex1 import html
+
+# Fill in the blank
+xpath = '//p'
+
+'''
+O código acima demonstra como usar XPath para selecionar todos os elementos <p> em um documento HTML. A expressão '//p' , com duas barras, indica que queremos selecionar todos os elementos <p> em qualquer nível da hierarquia do documento.
+'''
